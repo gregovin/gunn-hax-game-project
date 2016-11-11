76612e055296a61a821a00cb08a06e74aa9ae769
gregovin <gregory.w.hess@gmail.com>
2016-11-11 14:19:11 -0800

    Update and rename game.java to player.java

---
+++ player.java
@@ -0,0 +1,11 @@
+class player {
+  private double pos[] = new double[2]
+  private double vel[] = new double[2];
+  private double rot;
+  public player(double pos1[], double vel1[], double rot1) {
+    pos = pos1;
+    vel = vel1;
+    rot = rot1;
+  }
+  public 
+}
