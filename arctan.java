ac47f92c2ccf6f8bd391f3db330b7aacca74d076
gregovin <gregory.w.hess@gmail.com>
2016-11-11 14:47:28 -0800

    Update arctan.java

--- arctan.java
+++ arctan.java
@@ -1,3 +1,18 @@
 class arctan {
-
+  private double storage
+  public arctan(double y, double x) {
+    if (x==0 && y==0){
+      storage = 0;
+    }else if (x==0 && y>0){
+      storage = math.PI/2;
+    }else if (x==0 && y<0){
+      storage = -math.PI/2
+    }else if (y<0){
+      storage = -math.atan(-y/x)
+    }else:
+      storage = math.atan(y/x)
+  }
+  public double get_result() {
+    return storage
+  }
 }
