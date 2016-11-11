e31c07800ab8791a3eb8179de2671352432bab4c
gregovin <gregory.w.hess@gmail.com>
2016-11-11 15:10:46 -0800

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -17,4 +17,16 @@ class enemy {
     arctan v = new arctan(vel[1], vel[0]);
     rot = v.get_result();
   }
+  public void change_vel(double x, double y){
+    double dist[] = new double[2];
+    switch (motion_type){
+      case 0:
+        dist[0] = x - pos[0];
+        dist[1] = y - pos[1];
+        arctan v = new arctan(dist[1], dist[0]);
+        double rotation = v.get_result();
+        vel[0] = 5 * Math.sin(rotation);
+        vel[1] = 5 * Math.cos(rotation);
+    }
+  }
 }
