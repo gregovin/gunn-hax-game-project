82bdc3c64b061231712eb0180b21f166896781f6
gregovin <gregory.w.hess@gmail.com>
2016-11-11 15:53:09 -0800

    Update game.java

--- game.java
+++ game.java
@@ -7,7 +7,7 @@ class game {
   public static void main (String args[]) {
     while (0<1) {
       if (rand.nextInt((v.getTime - k)/100 +11) > 10 && ls.size < 100) {
-        ls.add(Enimy())
+        ls.add(Enimy(rand.nextInt(5),0,[rand.nextInt(400),rand.nextInt(400)]))
       }
     }
   }
