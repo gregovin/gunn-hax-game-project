f22208fe394c8e38b48cb8a1f8e277edcfcf4f3d
gregovin <gregory.w.hess@gmail.com>
2016-11-11 15:45:11 -0800

    Update game.java

--- game.java
+++ game.java
@@ -1,7 +1,14 @@
+import java.util.Date
 class game {
-  public static void main(String args[]) {
-    while (true) {
-      
+  Timestamp v = new Timestamp();
+  Random rand = new Random();
+  Enimy 
+  int k = v.getTime();
+  public static void main (String args[]) {
+    while (0<1) {
+      if (rand.nextInt((v.getTime - k)/100 +11) > 10) {
+        
+      }
     }
   }
 }
