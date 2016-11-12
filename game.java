9e72c0db4c7d8d5d2cabc64e00bacff9dff3006c
gregovin <gregory.w.hess@gmail.com>
2016-11-11 18:41:42 -0800

    Update game.java

--- game.java
+++ game.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
-import java.util.Date
-class game {
+import java.util.Date;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+class Game extend StateBasedGame{
+  public Game(gamename){
+    super(gamename);
+    this.addState(new Only(0));
+  }
+  @Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		this.getState(0).init(arg0, this)
+	}
   Timestamp v = new Timestamp();
   Player player = new Player([200, 200], [0, 0], 0)
   Random rand = new Random();
@@ -8,10 +18,10 @@ class game {
   player.getInputMap().put(KeyStroke.getKeyStroke("s"), "mov down")
   player.getInputMap().put(KeyStroke.getKeyStroke("a"), "mov left")
   player.getInputMap().put(KeyStroke.getKeyStroke("d"), "mov right")
-  player.getActionMaP().put("mov up", velCange0(-1))
-  player.getActionMaP().put("mov down", velCange0(1))
-  player.getActionMaP().put("mov left", velCange1(-1))
-  player.getActionMaP().put("mov right", velCange1(1))
+  player.getActionMap().put("mov up", velCange0(-1))
+  player.getActionMap().put("mov down", velCange0(1))
+  player.getActionMap().put("mov left", velCange1(-1))
+  player.getActionMap().put("mov right", velCange1(1))
   Enimy ls[] = new Enimy[100]
   double x[] = new double[5]
   int k = v.getTime();
