be574619035a05870b09febb16c467a57ddc46be
gregovin <gregory.w.hess@gmail.com>
2016-11-11 16:48:00 -0800

    Update game.java

--- game.java
+++ game.java
@@ -1,6 +1,4 @@
-import java.awt.KeyEventDispatcher;
-import java.awt.KeyboardFocusManager;
-import java.awt.event.KeyEvent;
+import javax.swing.*;
 import java.util.Date
 class game {
   Timestamp v = new Timestamp();
@@ -8,13 +6,15 @@ class game {
   Random rand = new Random();
   Enimy ls[] = new Enimy[100]
   int k = v.getTime();
+  KeyStroke keys[] = new KeyStroke[20]
+  int k;
   public static void main (String args[]) {
     while (0<1) {
       if (rand.nextInt((v.getTime - k)/100 +11) > 10 && ls.size < 100) {
         ls.add(Enimy(rand.nextInt(5),0,[rand.nextInt(400),rand.nextInt(400)]));
       }
-      player.update();
-      
+      player.getInputMap().put(KeyStroke.getKeyStroke("w"), "mov up")
+      player.getActionMaP().put("mov up", velCange0(1))
     }
   }
 }
